a9b2b38dd3b0d3e00d83c07a60ba7aa5cfb20af1
rakib02cse <112302822+rakib02cse@users.noreply.github.com>
2022-08-31 10:45:04 +0600

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main()  {
+  runApp(MyApp());
+}
+
+void runApp(MyApp myApp) {
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child:Text("Helo World") ,
+        ),
+      ),
+      
+    );
+  }
+}
